9ef4d886e0409841ecb8504c0e2b1a2df6960d81
0xSacha <sacha.graff@icloud.com>
2022-04-30 18:56:17 +0200

    add vaultLib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,43 @@ import { ConnectWallet } from '~/components/ConnectWallet'
 import { IncrementCounter } from '~/components/IncrementCounter'
 import { TransactionList } from '~/components/TransactionList'
 import { useCounterContract } from '~/hooks/counter'
+import { useVaultLibContract } from '~/hooks/vaultLib'
 
 const Home: NextPage = () => {
   const { contract: counter } = useCounterContract()
+  const { contract: vaultLib } = useVaultLibContract()
 
   const { data: counterResult } = useStarknetCall({
     contract: counter,
     method: 'counter',
     args: [],
   })
+
+  const { data: fundName } = useStarknetCall({
+    contract: vaultLib,
+    method: 'getName',
+    args: [],
+  })
+
+  const fundNameVaue = useMemo(() => {
+    if (fundName && fundName.length > 0) {
+      const value = toBN(fundName[0])
+      return value.toString(10)
+    }
+  }, [fundName])
+
+  // const { data: sharesTotalSupply } = useStarknetCall({
+  //   contract: vaultLib,
+  //   method: 'sharesTotalSupply',
+  //   args: [],
+  // })
+
+  // const sharesTotalSupplyValue = useMemo(() => {
+  //   if (sharesTotalSupply && sharesTotalSupply.length > 0) {
+  //     const value = toBN(counterResult[0])
+  //     return value.toString(10)
+  //   }
+  // }, [sharesTotalSupply])
 
   const counterValue = useMemo(() => {
     if (counterResult && counterResult.length > 0) {
@@ -31,6 +59,9 @@ const Home: NextPage = () => {
       <p>Address: {counter?.address}</p>
       <p>Value: {counterValue}</p>
       <IncrementCounter />
+      <h2>VaultLib Contract</h2>
+      <p>Address: {vaultLib?.address}</p>
+      <p>fund Name: {fundNameVaue}</p>
       <h2>Recent Transactions</h2>
       <TransactionList />
     </div>
